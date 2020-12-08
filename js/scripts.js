3a63fed8dbc1a76b289e767a9967d2413fc7cb54
jjwoolridge <jyoti.pferd@gmail.com>
2020-12-08 07:22:30 -0500

    Corrected syntax errors in js
    changed names to strings, removed declarations from individual array components.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 let pokemonList = [];
 
-let pokemonList[0] = {
-  namme: Eevee,
+pokemonList[0] = {
+  namme: 'Eevee',
   height: 0.3,
   type: 'field'
 }
 
-let pokemonList[1] = {
-  name: Vulpix,
+pokemonList[1] = {
+  name: 'Vulpix',
   height: 0.6,
   type: 'field'
 }
 
-let pokemonList[2] = {
-  name: Ponyta,
+pokemonList[2] = {
+  name: 'Ponyta',
   height: 1.0,
   type: 'field'
 }
 
-let pokemonList[3]= {
-  name = Chansey,
+pokemonList[3]= {
+  name: 'Chansey',
   height: 1.1,
-  type= 'fairy'
+  type: 'fairy'
 }
